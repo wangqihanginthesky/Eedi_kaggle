5ca78f27bd8297a42d280095c45af596f4cbf731
王啓航 <wangqihang@wangqihangdeMacBook-Pro.local>
2024-12-21 21:54:37 +0900

    update prepare data

---
+++ src/chain_of_thought/generate_cot.py
@@ -0,0 +1,4 @@
+def infer_cot(df):
+    """infet cot logic"""
+    df["p000-qwen25-32b-instruct-cot_misunderstanding"] ="dummy"
+    return df
---
+++ src/synthetic_data_generation/generation1.py
@@ -0,0 +1,4 @@
+def generate_1(train):
+    """few shot examples: randomly sampled from train.csv"""
+    tmp = train
+    return tmp
